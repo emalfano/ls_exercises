447c5330a84edfc55bb9ac1952eebd3ffc6630af
Elizabeth Alfano <emalfano1@gmail.com>
2017-02-09 03:42:51 +0000

    finished arrays exercises

---
+++ ruby_basics/arrays/a5.rb
@@ -0,0 +1,8 @@
+#Use Array#each to iterate over colors and print each element.
+#Expected output:
+#I'm the color red!
+
+colors = ['red', 'yellow', 'purple', 'green']
+colors.each  { |x|
+  puts "I'm the color #{x}!"
+}
