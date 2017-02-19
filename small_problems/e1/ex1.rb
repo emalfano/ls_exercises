12959cdbc2baabf8d47ba8c869aa7ae2d9d76b4a
Elizabeth Alfano <emalfano1@gmail.com>
2017-02-19 19:43:31 +0000

    initial commit for small problems

---
+++ small_problems/e1/ex1.rb
@@ -0,0 +1,10 @@
+# Repeat Yourself
+# Write a method that takes two arguments, a string and a positive integer, and 
+# prints the string as many times as the integer indicates.
+
+def repeat(string,int)
+  int.times do 
+    puts string
+  end
+end
+repeat('Howdy',3)
